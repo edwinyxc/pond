804722990d0f1af0368bbc90bab103b419772316
ed <edwin_yxc@163.com>
2014-08-14 11:25:52 +0800

    fix criterion

---
+++ core/src/test/java/com/shuimin/pond/core/FlyTest.java
@@ -0,0 +1,12 @@
+package com.shuimin.pond.core;
+
+/**
+ * Created by ed on 8/14/14.
+ */
+public class FlyTest {
+    public static void main(String[] args) {
+        System.out.println(
+                String.format("%s LIKE '%\\?%'", "s")
+        );
+    }
+}
