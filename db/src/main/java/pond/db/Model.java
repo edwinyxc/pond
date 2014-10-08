123af3bbde8f4750638cd69d9e204f99ff35344e
ed <ed@garden-crunch>
2014-10-08 14:54:17 +0800

    add Model

--- db/src/main/java/pond/db/Model.java
+++ db/src/main/java/pond/db/Model.java
@@ -1,39 +1,21 @@
 package pond.db;
 
 import static pond.common.S._try;
-
+import java.util.Map;
+import java.util.HashMap;
 /**
  * Created by ed on 9/29/14.
  */
 public class Model extends AbstractRecord {
+        
+    private static Map<Class,RecordService> daos = new HashMap();
 
-    public static <E extends Model> RecordService<E> dao(){
-        RecordService<E> service =
-                _try(() ->
-                        RecordService.build((Class<E>) _class()));
-        return service;
-    }
-
-    private static <T extends Model> Class<T> _class() throws ClassNotFoundException {
-        return (Class<T>) Class.forName(getClassName());
-    }
-
-
-    private static String getClassName() {
-        return new ClassGetter().getClassName();
-    }
-
-    static class ClassGetter extends SecurityManager {
-        public String getClassName() {
-            Class[] classes = getClassContext();
-            for (Class clazz : classes) {
-                if (Model.class.isAssignableFrom(clazz) && !clazz.equals(Model.class)) {
-                    return clazz.getName();
-                }
-            }
-            throw new RuntimeException(
-                    "failed to determine Model class name, are you sure models have been instrumented?");
+    public static <E extends Model> RecordService<E> dao(Class cls){
+        RecordService<E> eServ = (RecordService<E>) daos.get(cls);
+        if( eServ == null ){
+            eServ = RecordService.build( cls );
+            daos.put(cls,eServ);
         }
+        return eServ;
     }
-
 }
--- db/src/test/java/pond/db/ModelTest.java
+++ db/src/test/java/pond/db/ModelTest.java
@@ -6,22 +6,25 @@ import pond.common.sql.Criterion;
 import java.util.List;
 
 import static pond.common.S._for;
+import static pond.db.Model.*;
 
+import org.junit.Test;
 /**
  * Created by ed on 9/29/14.
  */
 public class ModelTest {
 
     public static void test2() {
-        List<Model> list =  TestModel.dao().query();
+        List<Model> list = dao(TestModel.class).query();
         S.echo(S.dump(_for(list).map(Model::view).toList()));
     }
 
 
-    public static void main(String[] args){
+    @Test
+    public void test() {
         test2();
+    }
 //        test3();
 //        test4();
 //        test5();
-    }
 }
