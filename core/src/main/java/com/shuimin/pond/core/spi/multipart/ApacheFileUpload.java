6832a5a1e676170723b6c6c02aae9deb1cb76a6b
ed <ed@garden-crunch>
2014-10-08 14:17:29 +0800

    ApacheFileUpload fix

--- core/src/main/java/com/shuimin/pond/core/spi/multipart/ApacheFileUpload.java
+++ core/src/main/java/com/shuimin/pond/core/spi/multipart/ApacheFileUpload.java
@@ -35,8 +35,10 @@ public class ApacheFileUpload implements MultipartRequestResolver {
 
         public ReqCtx(Request req) {
             this.encoding = _notNullElse(req.characterEncoding(), "UTF-8");
-            this.contentType = req.header("Content-Type")[0];
-            this.contentLength = S.parse.toLong(req.header("Content-Length")[0]);
+            this.contentType =_notNullElse(req.header("Content-Type"),
+                    new String[]{"text/html"})[0];
+            this.contentLength = S.parse.toLong(_notNullElse(req.header("Content-Length"),
+                        new String[]{"0"})[0]);
             try {
                 this.in = req.in();
             } catch (IOException e) {
