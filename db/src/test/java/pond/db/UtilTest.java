6998a7b54bb1d91099baa7a691b8690737d90482
edwinyxc <edwinyxc@gmail.com>
2015-08-14 14:32:50 +0800

    add throw statement to JDBCTmpl#getType

--- db/src/test/java/pond/db/UtilTest.java
+++ db/src/test/java/pond/db/UtilTest.java
@@ -3,6 +3,8 @@ package pond.db;
 import org.junit.Test;
 import pond.common.S;
 
+import java.util.Collections;
+
 import static org.junit.Assert.*;
 
 public class UtilTest {
@@ -11,5 +13,12 @@ public class UtilTest {
     public void testA(){
         String a = String.format("jdbc:mysql://%s/%s?%s", "ssss","weee","wwww");
         assertEquals("jdbc:mysql://ssss/weee?wwww",a);
+    }
+
+
+    @Test
+    public void test(){
+        Object a = Collections.emptyMap().get("ss");
+        System.out.print(a);
     }
 }
