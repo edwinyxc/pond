44c89d20900a53e0c899fa41a363128be10988a1
edwinyxc <edwinyxc@gmail.com>
2016-04-24 15:00:02 +0800

    DB#SQL#SQLWHERE

--- db/src/main/java/pond/db/sql/SqlUpdate.java
+++ db/src/main/java/pond/db/sql/SqlUpdate.java
@@ -5,7 +5,7 @@ import pond.common.f.Tuple;
 /**
  * Created by ed on 2014/4/30.
  */
-public interface SqlUpdate extends Sql, SqlWhere {
+public interface SqlUpdate extends Sql, SqlWhere<SqlUpdate> {
   public SqlUpdate set(Tuple<String, Object>... sets);
 
   public SqlUpdate set(String... sets);
